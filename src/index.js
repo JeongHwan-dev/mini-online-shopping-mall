cd2bc424f129f0f140cf1b31057cb3501c50d22b
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-07-23 18:11:32 +0900

    Update : parcel 번들러 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Fetch the items from the JSON file
 function loadItems() {
-  return fetch("data/data.json")
+  return fetch("./data/data.json")
     .then((response) => response.json())
     .then((json) => json.items);
 }
@@ -49,4 +49,4 @@ loadItems()
     displayItems(items);
     setEventListeners(items);
   })
-  .catch(console.log);
+  .catch(console.log("error"));
